ad45d5fe84e27be8a4ffa58692b8e529ed070be5
Julio Gtz <jc.gutierrezq@gmail.com>
2022-03-24 16:55:37 -0700

    postgres config heroku

--- database.py
+++ database.py
@@ -1,21 +1,27 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 # sqlite connection
-SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
+#SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
 
 # postgres connection
-#SQLALCHEMY_DATABASE_URL = "postgresql://postgres:12345@localhost/postgres"
+SQLALCHEMY_DATABASE_URL = os.environ.get('DATABASE_URL')
+
+if SQLALCHEMY_DATABASE_URL.starswith("postgres://"):
+    SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace("postgres://", "postgresql:", 1)
+
+
 
 # mysql connection
 #SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:test1234!@127.0.0.1:3306/todoapp"
 
 engine = create_engine(
     #solo para sqlite
-    SQLALCHEMY_DATABASE_URL, connect_args = {"check_same_thread": False}
+    #SQLALCHEMY_DATABASE_URL, connect_args = {"check_same_thread": False}
    
-    #SQLALCHEMY_DATABASE_URL
+    SQLALCHEMY_DATABASE_URL
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
