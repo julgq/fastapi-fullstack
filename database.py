e686f5e8c38c2e07a549878de39ffbb17ad02bae
Julio Gtz <jc.gutierrezq@gmail.com>
2022-03-24 17:25:30 -0700

    database config

--- database.py
+++ database.py
@@ -9,10 +9,10 @@ from sqlalchemy.ext.declarative import declarative_base
 # postgres connection
 #SQLALCHEMY_DATABASE_URL = "postgresql://postgres:12345@localhost/postgres"
 
-SQLALCHEMY_DATABASE_URL = str(os.environ.get('DATABASE_URL'))
+SQLALCHEMY_DATABASE_URL = os.environ.get('DATABASE_URL')
 
-if SQLALCHEMY_DATABASE_URL.startswith("postgres://"):
-    SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace("postgres://", "postgresql:", 1)
+"""if SQLALCHEMY_DATABASE_URL.startswith("postgres://"):
+    SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace("postgres://", "postgresql:", 1)"""
 
 
 # mysql connection
